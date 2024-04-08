5e2fd59a3ba276036d666ea5f33d6174cd555968
Anna Sold <anutka9602@mail.ru>
2024-04-08 11:26:35 +0300

    рыба фугу

--- src/zoo/fishes/Fugu.java
+++ src/zoo/fishes/Fugu.java
@@ -1,7 +1,30 @@
 package zoo.fishes;
 
 public class Fugu extends Fish {
+    double length;
+    int toxicity;
 
+    public Fugu(double weight, double length, int toxicity) {
+        super(weight);
+        this.length = length;
+        this.toxicity = toxicity;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public int getToxicity() {
+        return toxicity;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setToxicity(int toxicity) {
+        this.toxicity = toxicity;
+    }
 
     public Fugu(double weight) {
         super(weight);
