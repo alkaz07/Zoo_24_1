d37d2ab163793c4b0c86058b29112280fa2db4e2
SerzhSeryozhka <Jk3103KS@yandex.ru>
2024-04-08 12:24:13 +0300

    Добавил конструктор гетторы,ту стринг.

--- src/zoo/fishes/HerringKing.java
+++ src/zoo/fishes/HerringKing.java
@@ -1,13 +1,35 @@
 package zoo.fishes;
 
-public class HerringKing extends Fish{
+public class HerringKing extends Fish {
     private String color; // серебристый с ярко-красным плавником
-    private String length; // 5-11 метров
-    public HerringKing(double weight) {
-        super(weight);
-    }
-    public void swim(){
+    private double length; // 5-11 метров
+
+    public void swim() {
         System.out.println("Плавает держа голову кверху и вращая гигантским веслообразным телом.");
-        System.out.println("Предпочитает держаться поближе к косякам сельди");
+//        System.out.println("Предпочитает держаться поближе к косякам сельди");
+    }
+
+    public HerringKing(double weight, String color, double length) {
+        super(weight);
+        this.color = color;
+        this.length = length;
+    }
+
+    public String getColor() {
+        this.color = "серебристый с ярко-красным плавником";
+        return color;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    @Override
+    public String toString() {
+        return "Сельдяной король: " +
+                 getColor()  +
+                ", длинна " + length +
+                ", isAlive=" + isAlive +
+                ", вес " + weight ;
     }
 }
