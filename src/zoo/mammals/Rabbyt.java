3b6ae340487686a87cf9ecf60476802c2aaf92a4
SerzhSeryozhka <Jk3103KS@yandex.ru>
2024-04-08 13:44:59 +0300

    Добавил возможность ходить и плавать.

--- src/zoo/mammals/Rabbyt.java
+++ src/zoo/mammals/Rabbyt.java
@@ -1,15 +1,20 @@
 package zoo.mammals;
-
-import zoo.mammals.Mammal;
-
-public class Rabbyt extends Mammal {
+import zoo.ISwim;
+import zoo.IWalk;
+public class Rabbyt extends Mammal implements ISwim, IWalk {
     public Rabbyt(double weight) {
         super(weight);
-
     }
-
     @Override
     public void move() {
-
+        System.out.println();
+    }
+    @Override
+    public void swim() {
+        System.out.println();
+    }
+    @Override
+    public void walk() {
+        System.out.println();
     }
 }
