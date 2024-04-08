d22de55c6529fdce302ce352a40e851d93fa211d
alkaz07 <alkaz07@yandex.ru>
2024-04-08 13:09:04 +0300

    ловим рыбу

--- src/zoo/birds/Penguin.java
+++ src/zoo/birds/Penguin.java
@@ -1,9 +1,11 @@
 package zoo.birds;
 
+import zoo.IFisherman;
 import zoo.ISwim;
 import zoo.IWalk;
+import zoo.fishes.Fish;
 
-public class Penguin extends Bird implements ISwim, IWalk {
+public class Penguin extends Bird implements ISwim, IWalk, IFisherman {
     public Penguin(String name, double weight, String featherColor) {
         super(name, weight, featherColor);
     }
@@ -26,5 +28,14 @@ public class Penguin extends Bird implements ISwim, IWalk {
     @Override
     public void walk() {
         System.out.println("пингвины ходят вперевалку");
+    }
+
+    @Override
+    public double doFishing(Fish fish) {
+        swim();
+        if(fish.getWeight() <= this.weight*0.1)
+            return fish.getWeight();
+        else
+            return 0;
     }
 }
